53d890b95ba153537289b93484176102aeb4c5cd
Vijay S <37852613+vijay043077@users.noreply.github.com>
2020-03-21 23:04:38 -0500

    commit2

--- PyBank/main.py
+++ PyBank/main.py
@@ -22,13 +22,29 @@ with open(csv_path) as csv_file:
     previous_row = int(first_row[1])
     change_in_profit = 0
     net_change = []
-    max_increase = {"date": first_row[0]},"value": 0}
+    profit_increase = {"Date": first_row[0], "Profit/Losses": 0}
+    profit_decrease = {"Date": first_row[0], "Profit/Losses": 0}
 
     # read each row after header row
 for row in csv_reader:
     total_months_count = total_months_count + 1
     net_total = net_total + int(row[1])
-    change_in_profit = int(row[1]) -1
+    change_in_profit = int(row[1]) -previous_row
+    previous_row =(row[1])
+
+    # change in profit "if" statement
+    if len(net_change)> 0 and change_in_profit> max(net_change):
+        profit_increase["Date"] = row[0]
+        profit_increase["Profit/Losses"] = change_in_profit
+    elif len(net_change) and change_in_profit< min(net_change):
+        profit_decrease["Date"] = row[0]
+        profit_decrease["Profit/Losses"] = change_in_profit
+
+    net_change.append(change_in_profit)
+
+    
+
+
 
 
 
