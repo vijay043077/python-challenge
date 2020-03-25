ba608769775e6764bea3ee74a1d61617630fcb6e
Vijay S <37852613+vijay043077@users.noreply.github.com>
2020-03-24 23:55:12 -0500

    Commit7

--- PyBank/main.py
+++ PyBank/main.py
@@ -29,47 +29,46 @@ with open(csv_path) as csv_file:
     # read each row after header row
     # count total number of months 
     # net total amount of "Profit/Losses"
-for row in csv_reader:
-    total_months_count = total_months_count + 1
-    net_total = net_total + int(row[1])
-    change_in_profit = int(row[1]) -previous_row
-    previous_row =(row[1])
+    for row in csv_reader:
+        total_months_count = total_months_count + 1
+        net_total = net_total + int(row[1])
+        change_in_profit = int(row[1]) -previous_row
+        previous_row = int(row[1])
 
-    # change in profit "if" statement
-    if len(net_change)> 0 and change_in_profit> max(net_change):
-        profit_increase["Date"] = row[0]
-        profit_increase["Profit_Increase"] = change_in_profit
-    elif len(net_change) and change_in_profit< min(net_change):
-        profit_decrease["Date"] = row[0]
-        profit_decrease["Profit_Decrease"] = change_in_profit
+        # change in profit "if" statement
+        if len(net_change)> 0 and change_in_profit> max(net_change):
+            profit_increase["Date"] = row[0]
+            profit_increase["Profit_Increase"] = change_in_profit
+            print(profit_increase)
+            
+        elif len(net_change) and change_in_profit< min(net_change):
+            profit_decrease["Date"] = row[0]
+            profit_decrease["Profit_Decrease"] = change_in_profit
+        
+        net_change.append(change_in_profit)
 
-    net_change.append(change_in_profit)
+    # average of net change formula
+    average_net_change = sum(net_change)/len(net_change) 
 
-# average of net change formula
-average_net_change = sum(net_change)/len(net_change) 
+    # print out financial summary
+    print(f"Total Months: {total_months_count}")
+    print(f"Total: ${net_total}")
+    print(f"Average Change: ${average_net_change: .2f}")
+    print("Greatest Increase in Profits:", profit_increase["Date"], "$", profit_increase["Profit_Increase"])
+    print("Greatest Decrease in Profits:", profit_decrease["Date"], "$", profit_decrease["Profit_Decrease"])
 
-# print out financial summary
-print(f"Total Months: {total_months_count}")
-print(f"Total: ${net_total}")
-print(f"Average Change: ${average_net_change: .2f}")
-print("Greatest Increase in Profits:", profit_increase["Date"], "$", profit_increase["Profit_Increase"])
-print("Greatest Decrease in Profits:", profit_decrease["Date"], "$", profit_decrease["Profit_Decrease"])
-
-# set a variable to print to txt file
-text_file= os.path.join("budget_results.txt")
-
-# open txt file
-sys.stdout= open(text_file, "w")
-
-# print budget results to txt file
-print(f"Total Months: {total_months_count}")
-print(f"Total: ${net_total}")
-print(f"Average Change: ${average_net_change: .2f}")
-print("Greatest Increase in Profits:", profit_increase["Date"], "$", profit_increase["Profit_Increase"])
-print("Greatest Decrease in Profits:", profit_decrease["Date"], "$", profit_decrease["Profit_Decrease"]) 
-
-sys.stdout.close()
-
-
-
+    # set a variable to print to txt file
+    text_file= os.path.join("budget_results.txt")
+    output_text = ""
+     # print budget results to txt file\n"
+    output_text +=f"Total Months: {total_months_count}\n"
+    output_text +=f"Total: ${net_total}\n"
+    output_text +=f"Average Change: ${average_net_change: .2f}\n"
+    output_text +=str({'Greatest Increase in Profits:', profit_increase['Date'], '$', profit_increase['Profit_Increase']})
+    output_text +=str({'Greatest Decrease in Profits:', profit_decrease['Date'], '$', profit_decrease['Profit_Decrease']})
+    with open("output.txt","w") as out_text:
+        out_text.write(output_text)
     
+
+
+
